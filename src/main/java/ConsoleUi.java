58441ec782ddca7f004c56567fd1545d1a9eb692
drakir89 <drakir89@hotmail.com>
2018-06-11 13:29:22 +0200

    reworked ConsoleUi class. Now based on a loop that loads the correct menu, rather than questions that ask new questions (the old method lead to recursion issues) The class is still not finished.

--- src/main/java/ConsoleUi.java
+++ src/main/java/ConsoleUi.java
@@ -7,63 +7,96 @@ public class ConsoleUi {
     private OpenWeatherMapApi owmApi;
     private CityWeather cityWeather = null;
 
-    private enum QuestionToRepeat {PICK_FIRST_CITY, CHANGE_CITY_SEARCH_METHOD, CITY_DETAILS}
-    private QuestionToRepeat questionToRepeat = QuestionToRepeat.PICK_FIRST_CITY;
+    private enum Menu {PICK_CITY, CITY_DETAILS}
+    private Menu nextMenu = Menu.PICK_CITY;
 
     private enum CitySearchMethod {NAME, NAME_AND_COUNTRY, ID}
     private CitySearchMethod currentCitySearchMethod = CitySearchMethod.NAME;
 
+    private enum TemperatureUnit {FAHRENHEIT, CELSIUS, KELVIN}
+    private TemperatureUnit temperatureUnit = TemperatureUnit.CELSIUS;
+
     private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    private boolean returnToMenu = true;
 
     public ConsoleUi(OpenWeatherMapApi givenOwmApi) {
         owmApi = givenOwmApi;
     }
 
-    private void repeatQuestion(){
-        switch (questionToRepeat){
-            case PICK_FIRST_CITY:
-                //TODO: insert question method here
-                break;
-            case CHANGE_CITY_SEARCH_METHOD:
-                changeCitySearchMethod();
+    public void beginConversation(){
+        System.out.println("Welcome to my weather app!");
+        while (returnToMenu){
+            displayMenu();
+        }
+    }
+
+    private String getUserInput(){
+        String input = "";
+        try {
+            input = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
+
+    private void displayMenu(){
+        switch (nextMenu){
+            case PICK_CITY:
+                //TODO: insert menu method here
                 break;
             case CITY_DETAILS:
-                //TODO: insert question method here
+                //TODO: insert menu method here
                 break;
+            default:
+                System.out.println("Failed to display menu");
+        }
+    }
+
+    private void pickCity(){
+        System.out.println("0: enter city name"); //TODO: change response depending on the city search method
+        System.out.println("1: change search method");
+        String input = getUserInput();
+        switch (input){
+            case "0":
+                //TODO: add method to get cityweather from the api
+                nextMenu = Menu.CITY_DETAILS;
+                break;
+            case "1":
+                changeCitySearchMethod();
+                break;
+            default:
+                rejectInput();
         }
     }
 
     private void rejectInput(){
         System.out.println("Sorry, that input is invalid. Please try again:");
-        repeatQuestion();
     }
 
     private void changeCitySearchMethod() {
-        try {
-            System.out.println("how should we find the city?");
-            System.out.println("0: by city name, eg \"london\"");
-            System.out.println("1: by name and country code, eg \"london, uk\"");
-            System.out.println("2: by OpenWeatherMap ID, eg \"2643743\"");
+        System.out.println("how should we find the city?");
+        System.out.println("0: by city name, eg \"london\"");
+        System.out.println("1: by name and country code, eg \"london, uk\"");
+        System.out.println("2: by OpenWeatherMap ID, eg \"2643743\"");
 
-            String input = reader.readLine();
-            switch (input) {
-                case "0":
-                    this.currentCitySearchMethod = CitySearchMethod.NAME;
-                    System.out.println("You chose to use the city name only");
-                    break;
-                case "1":
-                    this.currentCitySearchMethod = CitySearchMethod.NAME_AND_COUNTRY;
-                    System.out.println("You chose to use the city name and the country code");
-                    break;
-                case "2":
-                    this.currentCitySearchMethod = CitySearchMethod.ID;
-                    System.out.println("You chose to use the OpenWeatherMap ID number");
-                    break;
-                default:
-                    rejectInput();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String input = getUserInput();
+        switch (input) {
+            case "0":
+                this.currentCitySearchMethod = CitySearchMethod.NAME;
+                System.out.println("You chose to use the city name only");
+                break;
+            case "1":
+                this.currentCitySearchMethod = CitySearchMethod.NAME_AND_COUNTRY;
+                System.out.println("You chose to use the city name and the country code");
+                break;
+            case "2":
+                this.currentCitySearchMethod = CitySearchMethod.ID;
+                System.out.println("You chose to use the OpenWeatherMap ID number");
+                break;
+            default:
+                rejectInput();
         }
     }
 }
